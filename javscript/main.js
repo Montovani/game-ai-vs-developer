5fa9ca68e63d9e5e87844853678ff6ae08a4545d
Igor Montovani <igor.montovani@gmail.com>
2025-10-21 11:40:39 +0200

    Add startgame and main game loop

--- javscript/main.js
+++ javscript/main.js
@@ -1,1 +1,27 @@
-console.log('test')
+//Global Nodes
+const startGameBtnNode = document.querySelector('.start-btn')
+const gameRunScreenNode = document.querySelector('.game-run-screen')
+const initialScreenNode = document.querySelector('.initial-screen')
+
+
+// Event Listener
+startGameBtnNode.addEventListener('click', startGame)
+
+
+function startGame(){
+    initialScreenNode.style.display = "none"
+    gameRunScreenNode.style.display = "flex"
+    
+    // Start the main game loop
+    const mainGameLoopId = setInterval(gameRunning,1000/60)
+    
+}
+
+function gameRunning() {
+    console.log('game running at 60 fps.')
+}
+
+
+function gameLoop() {
+    console.log('looping of the game')
+}
